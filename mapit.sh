7bf91a5b94055904323e81326f8c2c5fef49639f
inxeoz <inxeoz@inxeoz.com>
2025-11-20 13:09:55 +0530

    update mapit for easy installation

--- mapit.sh
+++ mapit.sh
@@ -4,6 +4,8 @@ docker compose -f mapit_pwd.yml down --volumes
 
 export APPS_JSON_BASE64=$(base64 -w 0 apps_mapit.json)
 
+export FRONTEND_PORT=8881
+
 echo "############# APPS_JSON_BASE64 ################\n\n"
 
 echo $APPS_JSON_BASE64
@@ -14,3 +16,7 @@ echo "############# APPS_JSON_BASE64 ################\n\n"
 docker compose -f mapit_pwd.yml build
 
 docker compose -f mapit_pwd.yml up -d
+
+echo "Visit this Url"
+
+echo "http://localhost:$FRONTEND_PORT/"
