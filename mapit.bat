41efce8bcc4c7e9beb78b67cc4f07da4e82d4767
inxeoz <inxeoz@inxeoz.com>
2025-11-20 13:17:45 +0530

    docker status

--- mapit.bat
+++ mapit.bat
@@ -1,5 +1,13 @@
 @echo off
 
+docker info >nul 2>&1
+if %errorlevel%==0 (
+    echo Docker is running
+) else (
+    echo Docker is NOT running
+)
+
+
 docker compose -f mapit_pwd.yml down --volumes
 
 for /f "delims=" %%i in ('powershell -command "[Convert]::ToBase64String([IO.File]::ReadAllBytes('apps_mapit.json'))"') do set APPS_JSON_BASE64=%%i
