14684320c301a3af947f4d0aa52fac6c2caefcf1
inxeoz <inxeoz@inxeoz.com>
2025-11-24 12:21:06 +0530

    mahakaal app

--- mapit.sh
+++ mapit.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
+# Ask for frontend port (default: 8881)
+read -p "Enter FRONTEND_PORT [8881]: " FRONTEND_PORT
+FRONTEND_PORT=${FRONTEND_PORT:-8881}
+
 docker compose -f mapit_pwd.yml down --volumes
+
+export FRONTEND_PORT
 
 export APPS_JSON_BASE64=$(base64 -w 0 apps_mapit.json)
 
-export FRONTEND_PORT=8881
-
-echo "############# APPS_JSON_BASE64 ################\n\n"
-
-echo $APPS_JSON_BASE64
-
-echo "############# APPS_JSON_BASE64 ################\n\n"
-
+echo -e "############# APPS_JSON_BASE64 ################\n"
+echo "$APPS_JSON_BASE64"
+echo -e "\n############# APPS_JSON_BASE64 ################\n"
 
 docker compose -f mapit_pwd.yml build
-
 docker compose -f mapit_pwd.yml up -d
 
-echo "Visit this Url"
-
+echo "Visit this URL:"
 echo "http://localhost:$FRONTEND_PORT/"
